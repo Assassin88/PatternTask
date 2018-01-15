98d27eeb13b16306847436d2d4707fdf4597d7fb
Paul <5540832@mail.ru>
2018-01-15 19:45:00 +0300

    Singleton
    Singleton

---
+++ Singleton_Log/Loger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Log
+{
+    static class Loger
+    {
+        public static void LogerMassage(string msg)
+        {
+            using (FileStream fs = new FileStream("DataLog.txt", FileMode.Append, FileAccess.Write))
+            {
+                if (fs.CanWrite)
+                {
+                    string mess = msg + Environment.NewLine + DateTime.Now.ToString() + 
+                        Environment.NewLine + Environment.NewLine;
+                    byte[] bytesWr = Encoding.Unicode.GetBytes(mess);
+                    fs.Write(bytesWr, 0, bytesWr.Length);
+                }
+            }
+        }
+    }
+}
---
+++ Singleton_Log/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Singleton_Log
+{
+    class Program
+    {
+        //Реализовать сервис логирования сообщений в текстовый файл.
+        //Исключить возможность создания нескольких объектов сервиса.
+        //Путь к файлу настраивается пользователем.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Car.GetInstance();");
+            var car = Car.GetInstance();
+            Thread.Sleep(2000);
+
+            var car2 = Car.GetInstance();
+
+            Car.ReleaseSource();
+            Console.WriteLine("Car.ReleaseSource();");
+
+            car2 = Car.GetInstance();
+            Console.WriteLine("Car.GetInstance();");
+            Thread.Sleep(2000);
+
+            Car.ReleaseSource();
+            Console.WriteLine("Car.ReleaseSource();");
+
+            Console.ReadKey();
+        }
+    }
+}
