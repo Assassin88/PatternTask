4135286e7240f29ecca7ac806623c7e713d615dd
Assassin88 <5540832@mail.ru>
2018-02-04 19:08:03 +0300

    Task6
    Add sixth exam task for Patterns

---
+++ Task6/Task6_Chat/Task6_Chat/Chat.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Task6_Chat
+{
+    class Chat
+    {
+        private readonly ISet<User> _colleagues = new HashSet<User>();
+
+        public void SignOn(User participant)
+        {
+            _colleagues.Add(participant);
+        }
+
+        public void SendMessage(string message, string from)
+        {
+            foreach (var item in _colleagues)
+            {
+                item.RecieveMessage(message, from);
+            }
+        }
+    }
+}
---
+++ Task6/Task6_Chat/Task6_Chat/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task6_Chat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var PublicGroup = new Chat();
+            var ivan = new User("Ivan");
+            var lena = new User("Lena");
+            var jan = new User("Jan");
+
+            ivan.AddUser(PublicGroup);
+            lena.AddUser(PublicGroup);
+            jan.AddUser(PublicGroup);
+
+            ivan.Send("Hello my frieds!!!", PublicGroup);
+            Console.WriteLine();
+
+            var groupChat = ivan.LocalGroup(lena);
+            ivan.Send("Hello Lena!!!", groupChat);
+            lena.Send("Hello Ivan!!!", groupChat);
+
+            Console.ReadKey();
+
+        }
+    }
+}
